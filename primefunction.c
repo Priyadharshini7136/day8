c30fe3a44340233e1764553c047d7cabde202395
Priyadharshini7136 <priyadharshinis23aid@vetias.ac.in>
2025-05-20 14:58:32 +0530

    Update primefunction.c

--- primefunction.c
+++ primefunction.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
-void primeNumber(int x){
-    if(x%2==0 && x%3==0)
-    printf("%d isnot prime",x);
-    else
-    printf("%d is prime",x);
- 
+void primeNumber(int n){
+    int flag=0;
+    for(int i=2;i<n-1;i++){
+    if(n%1==0){
+        return 0;
+    }
+ }
+ return 1;
 }
 int main()
 {
-    primeNumber(19);
+    int x;
+    scanf("%d",&x);
+    int n=prime(x);
+    if(n==1)
+    printf("%d is aprime number",x);
+    else printf("%d is not prime,x")
 }
